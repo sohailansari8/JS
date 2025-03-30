9fa2837ac63a25b2258e38cf4104222226418e98
sohailansari9136@gmail.com <sohailansari9136@gmail.com>
2025-03-30 23:15:08 +0530

    Add .gitignore, update HTML to include favicon, and enhance 10loops.js with various loop examples

--- 10loops.js
+++ 10loops.js
@@ -15,3 +15,74 @@
 for (let i = 0; i < 5; i++) {
     console.log("Iteration number: " + i);
 }
+
+// write a js code to find the odd numbers between 1 to 15 
+
+for(let i = 0 ; i<=15 ; i++){
+    if(i%2 != 0){
+        console.log(i);
+    }
+}
+
+
+// write a js code to find the even numbers between 1 to 10 
+
+for(let i = 0 ; i<=10 ; i++){
+    if(i%2 == 0){
+        console.log(i);
+    }
+}
+
+
+// wrtie a program to print a multiplication table of 5 
+
+for(let i = 0 ; i<=10 ; i++){
+    let table = 5 ;
+    console.log("5 X", i , "=" , table*i)
+}
+
+
+// nested for loops 
+// nested means writing a element inside a element so we can also nest for loops 
+
+for(let i = 0 ; i <= 3 ; i++){
+    console.log(`the outer repetitions ${i}`)
+    for(let j = 0 ; j <= 3 ; j++){
+        console.log(j);
+    }
+}
+
+
+
+
+
+// while loop 
+//while loop will run until the given condition is true 
+console.log(`lets see the while loop`);
+let i =1 ;
+while(i<=5 ){
+    console.log(i)
+    i++
+}
+
+
+
+
+// break statement 
+// break statement is used to break out from a loop
+// It is used to terminate the loop prematurely when a certain condition is met.
+
+console.log("Example of break statement:");
+for (let i = 1; i <= 10; i++) {
+    if (i === 6) {
+        console.log("Breaking the loop at i =", i);
+        break; // Exit the loop when i equals 6
+    }
+    console.log(i);
+}
+
+
+//for of loop 
+// its is a recently added loop type so we need to check the compatibiltiy with the browser for example internet explorer browser will not support the for of loop 
+// but in maximum cases it will work 
+
