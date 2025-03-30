0139a468351442d587588950cfd8e76507ad1d7c
sohailansari9136@gmail.com <sohailansari9136@gmail.com>
2025-03-30 14:07:42 +0530

    Add new JavaScript file for array manipulation and string checks; update script reference in HTML

---
+++ 10loops.js
@@ -0,0 +1,17 @@
+// Loops are used in JavaScript to perform repeated tasks based on a condition. 
+// Conditions typically return true or false, and the loop will continue to execute as long as the condition evaluates to true.
+
+// About the 'for' Loop in JavaScript:
+// The 'for' loop is one of the most commonly used loops in JavaScript. 
+// It is used when the number of iterations is known beforehand. 
+// It consists of three parts: initialization, condition, and increment/decrement.
+
+// // Syntax of 'for' Loop:
+// for (initialization; condition; increment/decrement) {
+//     // Code to be executed in each iteration
+// }
+
+// Example of a 'for' Loop:
+for (let i = 0; i < 5; i++) {
+    console.log("Iteration number: " + i);
+}
